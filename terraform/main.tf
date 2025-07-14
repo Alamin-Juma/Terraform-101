5ef83288bd9a3cd4d66f833c471b5b47d5c0a6fd
ggt-alamin-juma <alamin.magoti@griffinglobaltech.com>
2025-07-14 17:42:03 +0300

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    key = "aws/ec2-deployment/terraform.tfstate"
+    bucket = "alamin-s3-bucket2342"
+    key    = "aws/ec2-deployment/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
@@ -15,12 +17,35 @@ provider "aws" {
   region = var.region
 }
 
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "key_name" {
+  description = "Name of the AWS key pair"
+  type        = string
+}
+
+variable "public_key" {
+  description = "Public key for AWS key pair"
+  type        = string
+}
+
+variable "private_key" {
+  description = "Private key for SSH connection"
+  type        = string
+  sensitive   = true
+}
+
 resource "aws_instance" "server" {
-  ami           = "ami-0a7d80731ae1b2435" # Replace with the desired AMI ID
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.deployer.key_name
+  ami                    = "ami-0a7d80731ae1b2435" # Replace with the desired AMI ID
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+
   connection {
     type        = "ssh"
     host        = self.public_ip
@@ -33,53 +58,40 @@ resource "aws_instance" "server" {
     Name = "Deployment"
   }
 }
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2_instance_profile"
   role = "EC2-ECR-AUTH"
-  
 }
 
-resource "aws_security_group" "name" {
+resource "aws_security_group" "maingroup" {
   name        = "deployer-sg"
   description = "Security group for deployer instance"
   vpc_id      = "vpc-028b1a033958b5b0f" # Replace with your VPC ID
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    description      = "All outbound traffic"
-    security_groups  = []
-    self             = false
-    prefix_list_ids  = []
-    ipv6_cidr_blocks = []
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "All outbound traffic"
   }
 
-  ingress = [
-    {
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = "SSH access"
-      security_groups  = []
-      self             = false
-      prefix_list_ids  = []
-      ipv6_cidr_blocks = []
-    },
-    {
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = "HTTP access"
-      security_groups  = []
-      self             = false
-      prefix_list_ids  = []
-      ipv6_cidr_blocks = []
-    }
-  ]
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH access"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP access"
+  }
 
   tags = {
     Name = "deployer-sg"
@@ -92,6 +104,6 @@ resource "aws_key_pair" "deployer" {
 }
 
 output "instance_public_ip" {
-  value = aws_instance.server.public_ip
+  value     = aws_instance.server.public_ip
   sensitive = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,19 +6,11 @@ variable "region" {
 }
 
 variable "key_name" {
-  description = "Name of the key pair"
-  type        = string
-  default     = "my-key-pair"
 }
 
 variable "public_key" {
-  description = "Public key for the key pair"
-  type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC..."
+  
 }
 
 variable "private_key" {
-  description = "Private key for the key pair"
-  type        = string
-  default     = "-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEA...\n-----END RSA PRIVATE KEY-----"
 }
