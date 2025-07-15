1e9b74c49e8c4674f45cf8552354a902ef1a8539
ggt-alamin-juma <alamin.magoti@griffinglobaltech.com>
2025-07-15 15:09:14 +0300

    deploy dockerImage to ECR then to EC2

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "alamin-s3-bucket2342"
+    bucket = "user-uploads-prodalamin"
     key    = "aws/ec2-deployment/terraform.tfstate"
     region = "us-east-1"
   }
@@ -18,7 +18,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "server" {
-  ami                    = "ami-0a7d80731ae1b2435" # Replace with the desired AMI ID
+  ami                    = "ami-0a7d80731ae1b2435"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
@@ -45,7 +45,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 resource "aws_security_group" "maingroup" {
   name        = "deployer-sg"
   description = "Security group for deployer instance"
-  vpc_id      = "vpc-012c818e861291e1c" # Replace with your actual VPC ID
+  vpc_id      = "vpc-0a0e2f21dac671587"
 
   egress {
     from_port   = 0
@@ -84,4 +84,26 @@ resource "aws_key_pair" "deployer" {
 output "instance_public_ip" {
   value     = aws_instance.server.public_ip
   sensitive = true
+}
+
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "key_name" {
+  description = "Name of the AWS key pair"
+  type        = string
+}
+
+variable "public_key" {
+  description = "Public key for AWS key pair"
+  type        = string
+}
+
+variable "private_key" {
+  description = "Private key for SSH connection"
+  type        = string
+  sensitive   = true
 }
